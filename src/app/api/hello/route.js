b17e511851df00a6c5938d28886e85bc732f46f7
le <lile_0126@126.com>
2025-04-27 09:59:59 +0800

    add

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -3,7 +3,26 @@
 export const runtime = 'edge'
 
 export async function GET() {
-  const responseText = 'Hello World'
+  const responseText = '你好aaaaa'
+
+  // In the edge runtime you can use Bindings that are available in your application
+  // (for more details see:
+  //    - https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-bindings-in-your-nextjs-application
+  //    - https://developers.cloudflare.com/pages/functions/bindings/
+  // )
+  //
+  // KV Example:
+  // const myKv = getRequestContext().env.MY_KV_NAMESPACE
+  // await myKv.put('suffix', ' from a KV store!')
+  // const suffix = await myKv.get('suffix')
+  // return new Response(responseText + suffix)
+
+  return new Response(responseText)
+}
+
+
+export async function POST() {
+  const responseText = '你好aaaaa'
 
   // In the edge runtime you can use Bindings that are available in your application
   // (for more details see:
